708407cbdc0142dd84f4d7039fdff16d1c9f7a0d
Giorgio Delgado <hi@gdelgado.ca>
2020-05-02 13:38:24 -0400

    Add footer UI component

---
+++ showcase/src/elm/UI/Footer.elm
@@ -0,0 +1,25 @@
+module UI.Footer exposing (footer)
+
+import Css exposing (..)
+import Html.Styled as Styled exposing (div, text)
+import Html.Styled.Attributes exposing (css)
+import UI.Typography exposing (commonTextStyles)
+
+
+footerStyles : List Css.Style
+footerStyles =
+    commonTextStyles ++
+        [ height (px 100)
+        , color (hex "#fff")
+        , backgroundColor (hex "#000")
+        , marginTop (px 100)
+        , paddingTop (px 50)
+        , paddingRight (px 50)
+        , paddingLeft (px 50)
+        , textAlign center
+        ]
+
+footer : Styled.Html msg
+footer =
+    Styled.footer [ css footerStyles ]
+        [ div [ ] [ text "Made with ❤ by greptail"] ]
