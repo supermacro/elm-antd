e644579e4e5d9c90009b1371bb883fe00241b8fe
Giorgio Delgado <hi@gdelgado.ca>
2020-07-01 12:59:19 -0400

    DRY the button tests

--- showcase/visual-tests/button.spec.js
+++ showcase/visual-tests/button.spec.js
@@ -1,76 +1,41 @@
 import { visit, checkCorrectComponentRendered  } from './test-utils'
 
 describe('Button', () => {
-  const simpleButton = 'SimpleButton'
-  describe(simpleButton, () => {
-    it('Renders correctly', () => {
-      visit(simpleButton, cy)
+  const buttons = [
+    'SimpleButton',
+    'PrimaryButton',
+    'DashedButton',
+    'TextButton',
+    'LinkButton'
+  ]
 
-      checkCorrectComponentRendered(simpleButton, cy)
+  buttons.forEach((btn) => {
+    describe(btn, () => {
+      it('Renders Correctly', () => {
+        visit(btn, cy)
 
-      cy.percySnapshot(simpleButton)
-    })
+        checkCorrectComponentRendered(btn, cy)
 
-    // Cannot trigger :hover CSS States via cypress
-    // Also, I do not agree with workarounds that are
-    // Chrome/Chromium specific, as Firefox and IE Edge
-    // shoul also be taken into consideration.
-    //
-    // https://github.com/cypress-io/cypress/issues/10
-    it.skip('Renders hover button with correct styles', () => {
-      const snapshotTag = `${simpleButton}-hover`
+        cy.percySnapshot(btn)
+      })
 
-      visit(simpleButton, cy)
+      // Cannot trigger :hover CSS States via cypress
+      // Also, I do not agree with workarounds that are
+      // Chrome/Chromium specific, as Firefox and IE Edge
+      // shoul also be taken into consideration.
+      //
+      // https://github.com/cypress-io/cypress/issues/10
+      // https://github.com/supermacro/elm-antd/issues/18
+      it.skip('Renders hover styles correctly', () => {
+        const snapshotTag = `${btn}-hover`
 
-      checkCorrectComponentRendered(simpleButton, cy)
+        visit(btn, cy)
 
-      cy.get('button').rightclick()
-      cy.percySnapshot(snapshotTag)
-    })
-  })
+        checkCorrectComponentRendered(btn, cy)
 
-  const primaryButton = 'PrimaryButton'
-  describe(primaryButton, () => {
-    it('Renders correctly', () => {
-      visit(primaryButton, cy)
-
-      checkCorrectComponentRendered(primaryButton, cy)
-
-      cy.percySnapshot(primaryButton)
-    })
-  })
-
-
-  const dashedButton = 'DashedButton'
-  describe(dashedButton, () => {
-    it('Renders correctly', () => {
-      visit(dashedButton, cy)
-
-      checkCorrectComponentRendered(dashedButton, cy)
-
-      cy.percySnapshot(dashedButton)
-    })
-  })
-
-  const textButton = 'TextButton'
-  describe(textButton, () => {
-    it('Renders correctly', () => {
-      visit(textButton, cy)
-
-      checkCorrectComponentRendered(textButton, cy)
-
-      cy.percySnapshot(textButton)
-    })
-  })
-
-  const linkButton = 'LinkButton'
-  describe(linkButton, () => {
-    it('Renders correctly', () => {
-      visit(linkButton, cy)
-
-      checkCorrectComponentRendered(linkButton, cy)
-
-      cy.percySnapshot(linkButton)
+        cy.get('button').rightclick()
+        cy.percySnapshot()
+      })
     })
   })
 })
