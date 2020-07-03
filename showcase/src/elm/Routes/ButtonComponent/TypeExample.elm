e5e5f2b9965bf37072da008b59a65e897b4ccb4e
Giorgio Delgado <hi@gdelgado.ca>
2020-07-02 21:02:20 -0400

    Create 'withSpace' component for simpler spacing

--- showcase/src/elm/Routes/ButtonComponent/TypeExample.elm
+++ showcase/src/elm/Routes/ButtonComponent/TypeExample.elm
@@ -2,12 +2,18 @@ module Routes.ButtonComponent.TypeExample exposing (example)
 
 import Ant.Button as Btn exposing (ButtonType(..), button, toHtml)
 import Ant.Space as Space exposing (SpaceDirection(..))
-import Html exposing (Html)
+import Html exposing (Html, div, span)
+import Html.Attributes exposing (style)
 
 
-horizontalSpace : List (Html msg) -> Html msg
-horizontalSpace =
-    Space.toHtml << Space.direction Horizontal << Space.space
+withSpace : Html msg -> Html msg
+withSpace element =
+    span
+        [ style "margin-bottom" "13px"
+        , style "display" "inline-block"
+        , style "margin-right" "13px"
+        ]
+        [ element ]
 
 
 example : Html msg
@@ -17,28 +23,33 @@ example =
             button "Primary Button"
                 |> Btn.withType Primary
                 |> toHtml
+                |> withSpace
 
         defaultButton =
             button "Default Button"
                 |> Btn.withType Default
                 |> toHtml
+                |> withSpace
 
         dashedButton =
             button "Dashed Button"
                 |> Btn.withType Dashed
                 |> toHtml
+                |> withSpace
 
         textButton =
             button "Text Button"
                 |> Btn.withType Text
                 |> toHtml
+                |> withSpace
 
         linkButton =
             button "Link Button"
                 |> Btn.withType Link
                 |> toHtml
+                |> withSpace
     in
-    horizontalSpace
+    div []
         [ primaryButton
         , defaultButton
         , dashedButton
