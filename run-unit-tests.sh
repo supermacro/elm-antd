4a96355038c43a852c625acb3bd9ad2b9bc514dd
Giorgio Delgado <hi@gdelgado.ca>
2020-07-03 16:23:43 -0400

    Do not fail CI when skipping tests

---
+++ run-unit-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# For some reason, elm-test emits a non-zero exit code when
+# tests are skipped over.
+# This script ensures that we return a 0 exit code when elm-test
+# exits with a warning about tests having been skipped over
+
+
+function check-failure () {
+    declare -i ELM_TEST_EXIT_CODE=$?
+    declare -i ELM_TEST_FAILURE_CODE=2
+    declare -i ELM_TEST_WARNING_CODE=3
+
+    if [ $ELM_TEST_EXIT_CODE -eq $ELM_TEST_FAILURE_CODE ]; then
+        exit 1
+    else
+        exit 0
+    fi
+}
+
+elm-test || check-failure
+
