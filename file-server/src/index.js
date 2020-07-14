6c9b026d7a7c73264a770e2149f738b770c1b362
Giorgio Delgado <hi@gdelgado.ca>
2020-07-13 20:14:50 -0400

    minor fixes and add app.yaml

--- file-server/src/index.js
+++ file-server/src/index.js
@@ -12,6 +12,10 @@ app.get('/health-check', (_, res) => {
   res.sendStatus(200)
 })
 
+const mode = process.env.NODE_ENV === 'production'
+  ? 'production'
+  : 'development'
+
 app.listen(8080, () => {
-  console.log('Listening on port 8080')
+  console.log(`Listening on port 8080 in ${mode} mode`)
 })
